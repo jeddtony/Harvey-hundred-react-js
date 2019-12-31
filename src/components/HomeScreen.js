e000a8bd23a1767a12b9585d7585f4b9c38cb142
jeddtony <jedtony.jt@gmail.com>
2019-12-31 15:16:10 +0100

    Change color of numbers to white

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -9,7 +9,7 @@ export default function HomeScreen(props) {
     return (
         <div className={props.gameOver ? "homescreen homescreen--visible" : "homescreen homescreen--hidden"}>
         <div className="homescreen__box">
-          <h1 className="homescreen__title">Emoji&nbsp;  Match</h1>
+          <h1 className="homescreen__title">Harvey &nbsp;  Hundreds</h1>
           <div className="homescreen__stats">
             Games Won: <strong className="homescreen__number" >{props.gamesWon}</strong>
           </div>
